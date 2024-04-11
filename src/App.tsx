647a8d4cf5265bc5a8ee736b64ca59eb7d01459c
Shaival Desai <dshaival59@gmail.com>
2024-04-11 18:30:14 +0530

    heart_disease

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LoginUser from './Registration/Login';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginUser from "./Pages/Registration/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import RegisterUser from "./Pages/Registration/Register";
+import Prediction from "./Pages/Prediction/Prediction";
 
 function App() {
   return (
-   <>
-   <LoginUser/>
-   
-   
-   </>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginUser />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/prediction" element={<Prediction />} />
+      </Routes>
+    </>
   );
 }
 
